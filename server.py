a5763e0bcbfc4b8ec93b7d05efc6e5f76fbe8837
Milenko <39630670+MrMilenko@users.noreply.github.com>
2025-05-19 20:02:59 -0400

    Update server.py
    Added UDP listener and cleaned up comments Mr. 'wetsocks'

--- server.py
+++ server.py
@@ -1,35 +1,30 @@
+
 #!/env/Python3.10.4
 #/MobCat (2024)
 
 import asyncio
 import websockets, socket, time, urllib.request, json
+import threading
 from discordrp import Presence
-from websockets.server import WebSocketServerProtocol as wetSocks # Yes, I thought it was funny.
+from websockets.server import WebSocketServerProtocol as wetSocks
 
-# Login and tell discord app we are a game and we are running.
-clientID = "1304454011503513600" # 'Playing Xbox' id. I wish we could set 'Playing Game Name' but we can't without API abuse.
+clientID = "1304454011503513600" # Discord client ID
 presence = Presence(clientID)
 
-# If you host your own ver of the DB, then you can point to it here
-# However I haven't made all the code of this public yet sooo...
 APIURL = "https://mobcat.zip/XboxIDs"
 CDNURL = "https://raw.githubusercontent.com/MobCat/MobCats-original-xbox-game-list/main/icon"
 
-# Get server ip because I'm to lazy to type it. It should only be manually set on the clients side anyways.
 def getIP():
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	try:
-		# doesn't even have to be reachable
 		s.connect(('10.255.255.255', 1))
 		IP = s.getsockname()[0]
 	except:
 		IP = '127.0.0.1'
 	finally:
 		s.close()
-
 	return '.'.join(IP.split('.'))
 
-# Helper func to lookup title ids
 def lookupID(titleID):
 	try:
 		with urllib.request.urlopen(f"{APIURL}/api.php?id={titleID}") as url:
@@ -40,36 +35,23 @@ def lookupID(titleID):
 			else:
 				XMID = '00000000'
 				TitleName = 'Unknown Title'
-			
 	except Exception as e:
 		print(e)
-		# Lol so yeah, I forgot. If you enter bad data into the api, it just re-directs to the docs page
-		# json.load() is trying to load this html docs page as json...
-		# also if we dump bad not "URL safe" data into urlopen like 'Hello world' that will crash too, soo cheap catch all hack go burr.
 		XMID = '00000000'
 		TitleName = 'Unknown Title'
-
 	return XMID, TitleName
 
-########################################################################################################################
 async def clientHandler(websocket: wetSocks):
 	try:
-		# The websocket is already open when this handler is called
 		print(f"{int(time.time())} {websocket.remote_address} Xbox connected!")
-		
-		# Handle messages until xbox disconnects
 		async for message in websocket:
-			#Debug message
 			print(f"{int(time.time())} {websocket.remote_address} {message}")
-			#TODO: We should filter user input, but for now fuck it we ball. API already filters input, so it would be just to protect server from bad input.
 			dataIn = json.loads(message)
-			#PLEASE NOTE: We are doing a XMID lookup based on a title id. this has a 50/50/90 % chance of working. a lot of the time PAL and NTSC have the same title id.
 			XMID, TitleName = lookupID(dataIn['id'])
 			inTitleID = dataIn['id'].upper()
 
-			# Default presence data
 			presenceData = {
-				"type": 0, #TODO: Figure out Competing, parties, and the "secret" join match. the latter cant be used with Title info button.
+				"type": 0,
 				"details": TitleName,
 				"timestamps": {
 					"start": int(time.time()),
@@ -82,22 +64,14 @@ async def clientHandler(websocket: wetSocks):
 				"instance": True,
 			}
 
-			# Only append the title info button if that title is indeed in the database
 			if XMID != "00000000":
 				presenceData["buttons"] = [{"label": "Title Info", "url": f"{APIURL}/title.php?{XMID}",}]
-			else:
-				# Only append custom title name if title is not in db, and we supply a custom name in the first place
-				if 'name' in dataIn.keys() and dataIn['name'] != '':
-					presenceData['details'] = dataIn['name']
-					TitleName = dataIn['name']
+			elif 'name' in dataIn and dataIn['name']:
+				presenceData['details'] = dataIn['name']
+				TitleName = dataIn['name']
 
-			# Now we have some game info.. hopefully.. we can set our discord presence
 			presence.set(presenceData)
 			print(f"{int(time.time())} Now Playing {dataIn['id']} ({XMID}) - {TitleName}")
-
-			#TODO: Maybe do a check / echo back to xbox for yes, the server got your message, we have set discord presence. or no something went wrong.
-			#await websocket.send(f"OK")
-			
 	except websockets.ConnectionClosedOK:
 		print(f"{int(time.time())} {websocket.remote_address} Client disconnected normally")
 	except websockets.ConnectionClosedError as e:
@@ -105,39 +79,75 @@ async def clientHandler(websocket: wetSocks):
 	finally:
 		if websocket.closed:
 			print(f"{int(time.time())} {websocket.remote_address} Connection closed. Presence cleared.")
-			presence.clear() # This kinda does work, just slow for some reason, like 5 secs slow.
-			#TODO: If no xbox connection in 10 mins, shutdown the server and clear presence.
-            # So you don't get stuck "playing halo" for 2 weeks even know your xbox is off.
+			presence.clear()
 
-########################################################################################################################
+# === UDP listener added here ===
+def listen_udp():
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.bind(('0.0.0.0', 1102))  # Same port as WebSocket
+	print("[UDP] Listening for raw relay packets on port 1102...")
+
+	while True:
+		data, addr = sock.recvfrom(1024)
+		try:
+			message = data.decode("utf-8").strip()
+			print(f"[UDP] From {addr}: {message}")
+			dataIn = json.loads(message)
+
+			XMID, TitleName = lookupID(dataIn['id'])
+			inTitleID = dataIn['id'].upper()
+
+			presenceData = {
+				"type": 0,
+				"details": TitleName,
+				"timestamps": { "start": int(time.time()) },
+				"assets": {
+					"large_image": f"{CDNURL}/{inTitleID[:4]}/{inTitleID}.png",
+					"large_text": f"TitleID: {dataIn['id']}",
+					"small_image": "https://cdn.discordapp.com/avatars/1304454011503513600/6be191f921ebffb2f9a52c1b6fc26dfa"
+				},
+				"instance": True,
+			}
+
+			if XMID != "00000000":
+				presenceData["buttons"] = [{"label": "Title Info", "url": f"{APIURL}/title.php?{XMID}"}]
+			elif 'name' in dataIn and dataIn['name']:
+				presenceData["details"] = dataIn['name']
+				TitleName = dataIn['name']
+
+			presence.set(presenceData)
+			print(f"[UDP] Now Playing {dataIn['id']} ({XMID}) - {TitleName}")
+
+		except Exception as e:
+			print(f"[UDP ERROR] {e}")
+
+# === Main async WebSocket server entry point ===
 async def main():
-	# Get server IP
 	serverIP = getIP()
-	# Create the websocket server
 	server = await websockets.serve(clientHandler, serverIP, 1102)
 	print(f"Server started on ws://{serverIP}:1102\nWaiting for connection...")
-	
+
+	threading.Thread(target=listen_udp, daemon=True).start()
+
 	try:
-		# run forever
-		await asyncio.Future()  
-	except KeyboardInterrupt: #BUGBUG: This shit doesn't work, but its the thought that counts... right?
+		await asyncio.Future()
+	except KeyboardInterrupt:
 		print("\nShutting down server...")
 	finally:
-		# Clean shutdown
 		presence.close()
 		server.close()
 		await server.wait_closed()
 		print("Server closed")
 		exit()
 
-########################################################################################################################
-# Server starts here.
+# Banner + launch
 print(r'''
       _         _ __ _         _       
 __  _| |__   __| / _\ |_  __ _| |_ ___ 
 \ \/ / '_ \ / _` \ \| __|/ _` | __/ __|
- >  <| |_) | (_| |\ \ |_  (_| | |_\__ \
+ >  <| |_) | (_| |\ \ |_  (_| | |_\__ \\
 /_/\_\_.__/ \__,_\__/\__|\__,_|\__|___/
 xbStats Server 20241111
 ''')
+
 asyncio.run(main())
